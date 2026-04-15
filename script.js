8019be2f8f4563727a635a765121dd19ee4d3a2d
riri242 <renam2337@gmail.com>
2026-04-16 02:49:34 +0300

    fix

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 const upload = document.getElementById("upload");
+
+const uploadScreen = document.getElementById("uploadScreen");
+const editorScreen = document.getElementById("editorScreen");
+
 const finalImage = document.getElementById("finalImage");
 const canvas = document.getElementById("canvas");
 
 /* رفع الصورة */
-upload.onchange = (e) => {
+upload.addEventListener("change", (e) => {
   const file = e.target.files[0];
   if (!file) return;
 
   const url = URL.createObjectURL(file);
-  finalImage.src = url;
-};
 
-/* تحميل الصورة (FIXED) */
+  // حط الصورة
+  finalImage.src = url;
+
+  // 🔥 التبديل بين الشاشات
+  uploadScreen.classList.add("hidden");
+  editorScreen.classList.remove("hidden");
+});
+
+/* تحميل الصورة */
 function downloadImage() {
-  if (!finalImage.src) return alert("ارفع صورة أولاً");
+  if (!finalImage.src) return;
 
   html2canvas(canvas, {
     useCORS: true,
     scale: 2
   }).then((canvasEl) => {
     const link = document.createElement("a");
-    link.download = "ip-event.png";
+    link.download = "techprotok.png";
     link.href = canvasEl.toDataURL("image/png");
     link.click();
   });
